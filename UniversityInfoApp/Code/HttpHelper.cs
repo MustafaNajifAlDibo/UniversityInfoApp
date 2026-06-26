a97dd3ec661bbc5c123640eacd7da7babd5eb29e
Mustafa Najif Al-Dibo <mustafa.najif@outlook.com>
2026-06-26 21:07:27 +0200

    Setup UniInfoModel andf HttpHelper

---
+++ UniversityInfoApp/Code/HttpHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityInfoApp.Code {
+    public class HttpHelper {
+
+        public async Task<string> GetReponceAsync(string url) {
+            using (HttpClient client = new HttpClient()) {
+                var response = await client.GetAsync(url);
+
+                if(response.IsSuccessStatusCode) {
+                    return await response.Content.ReadAsStringAsync();
+                } else {
+                    return string.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ UniversityInfoApp/Models/UniInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityInfoApp.Models {
+    public class UniInfo {
+
+        public string[] web_pages { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public string alpha_two_code { get; set; }
+        public string[] domains { get; set; }
+        public object stateprovince { get; set; }
+
+    }
+}
